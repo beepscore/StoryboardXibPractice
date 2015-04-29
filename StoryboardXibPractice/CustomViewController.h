4953da8fb49c3e7b12032330ff5322a053620086
beepscore <support@beepscore.com>
2015-04-28 22:59:21 -0700

    Add CustomViewController with xib. TODO: Use CustomViewController.

---
+++ StoryboardXibPractice/CustomViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomViewController.h
+//  StoryboardXibPractice
+//
+//  Created by Steve Baker on 4/28/15.
+//  Copyright (c) 2015 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomViewController : UIViewController
+
+@end
